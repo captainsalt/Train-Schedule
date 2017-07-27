398ce0b88295ccf3eb353356d26f046e29b21535
David Wright <dontbothertheking@gmail.com>
2017-07-27 03:34:50 -0500

    Default time set to today

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -2,7 +2,13 @@
     loginToDatabase();
     setInterval(updateTrainTime, 300);
 
-    //Submits the train to the database
+    //Set default time to today
+    var format = "YYYY-MM-DDThh:mm";
+    $("#arrivalText").val(moment(new Date()).format(format));
+
+    // yyyy - MM - ddThh:mm
+    //2017-07-27T03:02
+    // 2017 - 07 - 27T03: 23
     $("#submitButton").click(() => {
         event.preventDefault();
         var inputs = $("#trainForm div input").get();
